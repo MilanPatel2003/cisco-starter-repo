36cc6f4d2cdfe7a88430955e23d8b9fdd33f8443
MilanPatel2003 <131247110+MilanPatel2003@users.noreply.github.com>
2024-10-12 10:36:47 +0530

    banner creation

---
+++ sextant/src/components/Banner.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <section className="ban_sec">
+    <div className="container">
+        <div className="ban_img">
+            <img src="https://i.ibb.co/NVjsGWG/banner.jpg" alt="banner" border="0" />
+            <div className="ban_text">
+                <strong>
+                    <span>Sextant</span><br />
+                </strong>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <a href="#">Ut enim ad minim veniam</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+  )
+}
+
+export default Banner
